a546082fd49491840121fc9808a704237e70b4b0
Mirthe Winter <mirthe.winter@gnorrel.nl>
2026-02-20 19:04:23 +0100

    first version finished

--- src/main.py
+++ src/main.py
@@ -1,28 +1,21 @@
 import os
 import shutil
-from textnode import TextType, TextNode
-from htmlnode import HTMLNode, LeafNode, ParentNode
 from blocks import markdown_to_html_node
 
 
 def main():
-    # new_node = TextNode("dummy", TextType.TEXT)
-    # print(new_node)
     my_copy("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page(
-        "content/blog/glorfindel/index.md",
-        "template.html",
-        "public/blog/glorfindel/index.html",
-    )
-    generate_page(
-        "content/blog/majesty/index.md",
-        "template.html",
-        "public/blog/majesty/index.html",
-    )
-    generate_page(
-        "content/blog/tom/index.md", "template.html", "public/blog/tom/index.html"
-    )
+    generate_pages_recursive("content", "template.html", "public")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for file in os.listdir(dir_path_content):
+        file_path = f"{dir_path_content}/{file}"
+        dest_path = f"{dest_dir_path}/{file}"
+        if os.path.isdir(file_path):
+            generate_pages_recursive(file_path, template_path, dest_path)
+        elif len(file) > 3 and file[-3:] == ".md":
+            generate_page(file_path, template_path, f"{dest_path[:-3]}.html")
 
 
 def my_copy(src, dest):
@@ -30,13 +23,10 @@ def my_copy(src, dest):
     os.mkdir(dest)
     print(f"Copying files to {dest}...")
     for file in os.listdir(src):
-        print(file)
         if os.path.isdir(f"{src}/{file}"):
-            print("isdir somehow", file)
             os.mkdir(f"{dest}/{file}")
             my_copy(f"{src}/{file}", f"{dest}/{file}")
         else:
-            print("not a dir", file)
             shutil.copy(f"{src}/{file}", f"{dest}/{file}")
 
 
